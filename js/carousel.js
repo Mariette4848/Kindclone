deb295f698379922b5b60317d9a3d332a33249d9
marietouyalcouye@gmail.com <marietouyalcouye@gmail.com>
2017-06-16 11:44:48 +0000

    kind
    kind v1.0

---
+++ js/carousel.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){<font></font>
+  $('.slider1').bxSlider({<font></font>
+    slideWidth: 200,<font></font>
+    minSlides: 2,<font></font>
+    maxSlides: 3,<font></font>
+    slideMargin: 10<font></font>
+  });<font></font>
+});<font></font>
