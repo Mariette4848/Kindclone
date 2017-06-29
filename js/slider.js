a160a5538149fb65ac53971749d8f352c2aefc84
marietouyalcouye@gmail.com <marietouyalcouye@gmail.com>
2017-06-29 15:17:12 +0000

    kind v1.0

---
+++ js/slider.js
@@ -0,0 +1,16 @@
+$('.bxslider').bxSlider({
+  minSlides: 7,
+  maxSlides: 8,
+  slideWidth: 300,
+  slideMargin: 1
+});
+
+$(document).ready(function(){
+  $('.slider5').bxSlider({
+    slideWidth: 900,
+    minSlides: 3,
+    maxSlides: 3,
+     startSlide: 2,
+    slideMargin: 6
+  });
+});
